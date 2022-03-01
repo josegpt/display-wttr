9d66610d2bedbd4cb4c1965525e2611d5d0356d2
Jose G Perez Taveras <josegpt27@gmail.com>
2022-03-01 16:05:30 -0500

    test: Add test for display-wttr(-mode)

--- display-wttr-test.el
+++ display-wttr-test.el
@@ -5,3 +5,16 @@
 
 (ert-deftest happy-path-test ()
   (should (= (+ 1 2) 3)))
+
+(defun display-wttr-update ()
+  (setq display-wttr-list nil)
+  (display-wttr-filter "" "display-wttr")
+  (display-wttr-sentinel "" "finished\n"))
+
+(ert-deftest display-wttr-test ()
+  (display-wttr)
+  (should (memq 'display-wttr-string global-mode-string))
+  (should (equal display-wttr-string "display-wttr "))
+  (should (equal display-wttr-list '("display-wttr")))
+  (should (= (aref display-wttr-timer 4) 3600))
+  (should (equal (aref display-wttr-timer 5) 'display-wttr-update-handler)))
