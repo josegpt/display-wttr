51d7902367158aec8ebb79dccdda072dc675d98d
Jose G Perez Taveras <josegpt27@gmail.com>
2022-03-06 11:53:05 -0500

    docs: Expand commentary about package

--- display-wttr.el
+++ display-wttr.el
@@ -11,7 +11,13 @@
 
 ;;; Commentary:
 
-;; Heavily inspired by: display-time-mode
+;; Display-wttr package contains a minor mode that can be toggled
+;; on/off.  It fetches weather information based on your
+;; location or a location set in `display-wttr-location' from
+;; `https://wttr.in' and then displays it on the mode line.  The entry
+;; point is`display-wttr'.
+
+;; Heavily inspired by: `display-time'.
 
 ;;; Code:
 
