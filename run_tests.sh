a355e67c8b82ceadf74c1c2b654d5ca3c1d869dd
Jose G Perez Taveras <josegpt27@gmail.com>
2022-03-06 16:12:57 -0500

    test: Add package-lint

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,20 @@
 
 EMACS="${EMACS:=emacs}"
 
-${EMACS} --batch -l display-wttr.el -l display-wttr-test.el -f ert-run-tests-batch-and-exit
+NEEDED_PACKAGES="package-lint"
+
+INIT_PACKAGE_EL="(progn \
+  (require 'package) \
+  (push '(\"melpa\" . \"https://melpa.org/packages/\") package-archives) \
+  (package-initialize) \
+  (unless package-archive-contents \
+     (package-refresh-contents)) \
+  (dolist (pkg '(${NEEDED_PACKAGES})) \
+    (unless (package-installed-p pkg) \
+      (package-install pkg))))"
+
+# Refresh package archives, because the test suite needs to see at least
+# package-lint.
+"$EMACS" -Q -batch \
+         --eval "$INIT_PACKAGE_EL"
+
